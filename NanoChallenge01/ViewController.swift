f04e1deae32417963063dee66e28c061d3db5b1a
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-04-26 12:06:25 +0700

    Initial Commit

---
+++ NanoChallenge01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NanoChallenge01
+//
+//  Created by Stephen Giovanni Saputra on 26/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
