7cf17225e532fc26a3f18ff6a66efe01aa12848f
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-11-23 20:49:40 +0700

    Added IQKeyboard for auto keyboard dismiss func

--- NanoChallenge01/AppDelegate.swift
+++ NanoChallenge01/AppDelegate.swift
@@ -7,12 +7,18 @@
 
 import UIKit
 import CoreData
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Hold app at launch screen
+        Thread.sleep(forTimeInterval: 1)
+        
+        IQKeyboardManager.shared.enable = true
+        
         return true
     }
 
